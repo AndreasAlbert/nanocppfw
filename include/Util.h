950c497640712c819b5c1a88ac2b05baeb411909
Andreas Albert <andreas.albert@cern.ch>
2019-03-21 13:37:45 +0100

    New util function.

--- include/Util.h
+++ include/Util.h
@@ -1,5 +1,8 @@
 #include <string>
+#include <vector>
 #include <algorithm>
+
+#include <TString.h>
 
 /// Taken from https://stackoverflow.com/a/12468109
 std::string random_string( size_t length )
@@ -16,4 +19,13 @@ std::string random_string( size_t length )
     std::string str(length,0);
     std::generate_n( str.begin(), length, randchar );
     return str;
+}
+
+
+vector<TString> string_to_tstrings(vector<string> strings) {
+    vector <TString> tstrings;
+    for(auto const istring : strings) {
+        tstrings.push_back(TString(istring));
+    }
+    return tstrings;
 }
