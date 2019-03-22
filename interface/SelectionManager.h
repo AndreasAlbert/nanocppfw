0570e471b195ffa8173cdcaa32727a0f956dfac0
Andreas Albert <andreas.albert@cern.ch>
2019-03-22 16:41:32 +0100

    Extend Selections truct

--- interface/SelectionManager.h
+++ interface/SelectionManager.h
@@ -19,11 +19,47 @@ struct Selection {
     TString tag;
     TString selection_string;
     bool blind;
-    Selection(TString tag, TString istring, bool iblind) {
+
+    // Default constructor
+    Selection() {
+        this->tag = "";
+        this->selection_string = "";
+        this->blind = false;
+    }
+
+    // Constructor for combination of selections
+    Selection(vector<Selection> selections) : Selection("","",false) {
+        for(auto const sel : selections){
+            *this+= sel;
+        }
+    }
+
+    // Constructor for combination of selections with custom tag
+    Selection(TString tag, vector<Selection> selections, bool blind) : Selection(selections){
+        this->tag = tag;
+        this->blind = blind;
+    }
+
+    // Constructor for fully manual definition
+    Selection(TString tag, TString istring, bool blind) {
         this->tag = tag;
         this->selection_string = istring;
-        this->blind = iblind;
+        this->blind = blind;
     }
+
+    // Sum operator simply combines selectors and tags
+    Selection operator+=(const Selection & rhs) {
+        this -> selection_string = "(" + this->selection_string + ") && (" + rhs.selection_string + ")";
+        this -> tag = "(" + this->tag + ") && (" + rhs.tag + ")";
+        this -> blind = (this->blind or rhs.blind);
+        return *this;
+    }
+    friend Selection operator+(Selection lhs, const Selection rhs){
+        lhs += rhs;
+        return lhs;
+    }
+
+
 };
 
 //// Class to implement event selection on an RDF
