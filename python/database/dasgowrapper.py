d86334251fa11e8ff847e937e35c55b782d1750c
Andreas Albert <andreas.albert@cern.ch>
2019-04-04 18:07:43 +0200

    dasgowrapper

--- python/database/dasgowrapper.py
+++ python/database/dasgowrapper.py
@@ -1,15 +1,33 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import subprocess
 
-def das_go_query(query):
-    proc = subprocess.run(
-        ["dasgoclient", "--query", query],
-        stdout=subprocess.PIPE)
-    if proc.returncode != 0:
-        raise RuntimeError(f"Could not run DAS Go client query: {query}")
-    rawlines =  proc.stdout.splitlines()
-    lines = list(map(lambda x: x.decode("utf-8").strip(), rawlines))
-    lines = [l for l in lines if l]
 
-    return lines
+def das_go_query(query, json=False):
+    cmd = ["dasgoclient", "--query", query]
+    if json:
+        cmd.append("-json")
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+    stdout, _ = proc.communicate()
+    if proc.returncode != 0:
+        raise RuntimeError("Could not run DAS Go client query: {}.".format(query))
+
+    return stdout
+
+
+
+
+def das_go_query_json(query):
+    proc = subprocess.Popen(
+        ["dasgoclient", "--query", query, "-json"],
+        stdout=subprocess.PIPE)
+    stdout, _ = proc.communicate()
+    if proc.returncode != 0:
+        raise RuntimeError("Could not run DAS Go client query: {}.".format(query))
+    return json.loads(stdout)
+    # rawlines =  stdout.splitlines()
+    # lines = list(map(lambda x: x.decode("utf-8").strip(), rawlines))
+    # lines = [l for l in lines if l]
+    # print lines
+    
