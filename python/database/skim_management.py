77cd5778c54ed9cad016b98ed8e4182b03c83dac
Andreas Albert <andreas.albert@cern.ch>
2019-04-04 18:08:15 +0200

    WIP: skim database

--- python/database/skim_management.py
+++ python/database/skim_management.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from sqlalchemy import String
 import luigi
@@ -7,7 +7,11 @@ from luigi.contrib import sqla
 from database_objects import Period, Dataset, Skim, File
 from dasgowrapper import das_go_query
 
-
+import os
+import tarfile
+import json
+import re
+import subprocess
 class skim(luigi.Config):
     cmssw_base = luigi.Parameter(default='')
 
@@ -35,83 +39,118 @@ class InitializeDatasets(sqla.CopyToTable):
 
         If the input path is already a fully formed path,
         the output list will simply contain that path as its
-        only element. If, however, there are asterisks in the 
+        only element. If, however, there are asterisks in the
         input, a DAS query will be used to find all data set
         names matching this pattern.
-        
+
         :raises RuntimeError: If DAS reports that no matching paths are found.
         """
-        paths = das_go_query(f"dataset={self.input_path}")
+        rawlines = das_go_query("dataset={}".format(self.input_path))
+
+        paths = list(map(lambda x: x.decode("utf-8").strip(), rawlines.splitlines()))
         if not len(paths):
-            raise RuntimeError(f"No datasets found for path {self.input_path}")
+            raise RuntimeError("No datasets found for path {}.".format(self.input_path))
         self.paths = paths
 
     def rows(self):
         self.expand_input_path()
+        print self.paths
         for ipath in self.paths:
+            # Get data set properties from DAS
+            props = json.loads(das_go_query("dataset={}".format(ipath), json=True))
+            data = {}
+            for entry in props:
+                data.update(entry["dataset"][0])
 
-            # Get number of events from DAS
-            lines = das_go_query(f"dataset={ipath} | grep dataset.nevents")
-            assert(len(lines)==1)
-            nevents = int(lines[0])
+            # Number of events in data set
+            nevents = int(data["nevents"])
 
-            # Deduce run period
-            if "RunIISummer16" in ipath:
-                period = "2016"
-            elif "RunIIFall17" in ipath:
-                period = "2017"
-            elif "RunIIAutumn18" in ipath:
-                period = "2018"
-            
             # MC or not?
-            is_mc = ipath.endswith("SIM")
+            is_mc = data["datatype"] != "data"
+
+            # Run period
+            period = data["acquisition_era_name"]
 
             # Dummy
             xs = None
+
             # Construct row
             irow = (ipath, is_mc, xs, nevents, period)
             yield irow
 
 
-class ExtractTarFile(luigi.Task):
+class ExtractTarMember(luigi.Task):
     """Task that extracts a given tar file
-    
-    :param filepath: Path to the tar file.
-    :type luigi: string
+
+    :param tarpath: Path to the tar file.
+    :type tarpath: string
+    :param member: Member of the tar file to extract.
+    :type member: string
     """
 
-    filepath = luigi.Parameter()
+    tarpath = luigi.Parameter()
+    member = luigi.Parameter()
 
     def run(self):
-        self.unpack_path = os.path.join(filepath, "untar")
+        tf = tarfile.open(self.tarpath)
+        tf.extract(self.member, path=os.path.dirname(self.tarpath))
 
-        os.makedirs(unpack_path)
-        tf = tarfile.open(filepath)
-        tf.extract(path=unpack_path)
-    
     def output(self):
-        return luigi.LocalTarget(path=self.unpack_path)
+        output_path = os.path.join(os.path.dirname(self.tarpath), self.member)
+        return luigi.LocalTarget(path=output_path)
 
-class PrepareSkimFolder(luigi.Task):
-    skim_folder = luigi.Parameter()
+
+import imp
+from crab_helpers import get_crab_files
+from luigi.mock import MockTarget
+
+class GetCrabFiles(luigi.Task):
+    """
+    Compiles output file info for a CRAB task
+
+    For each file, a line of information is written into a
+    mock target. The lines are correctly formatted to be fed
+    into the sqla File constructor.
+    """
+    crab_path = luigi.Parameter()
 
     def run(self):
+        # Read the config file to determine the dataset and skim tag
+        configfile = os.path.join(self.crab_path,"inputs/debug/crabConfig.py")
+        dummy =  imp.load_source('dummy', configfile)
 
-        debug_folder = pjoin(skim_folder,"input")
-        debug_file = pjoin(debug_folder,"debugFiles.tgz")
+        dataset = dummy.config.Data.inputDataset
+        skim_tag = dummy.config.Data.outputDatasetTag
 
-        tf = tarfile.open(debug_file)
-        tf.extract(path=debug_file)
+        # Make sure dataset is registered
+        yield InitializeDatasets(dataset)
 
-class RegisterSkimFolder(luigi.Task):
-    skim_folder = luigi.Parameter()
+        # Extract file list and write
+        rawline = "{path} {size} {events} {dataset} {skim_tag}\n"
+        out = self.output().open("w")
+        for path in get_crab_files(self.crab_path):
+            size = 0
+            events = 0
+            line = rawline.format(path=path, size=size, events=events, dataset=dataset, skim_tag=skim_tag)
+            out.write(line)
+        out.close()
 
+    def output(self):
+        return MockTarget("mymock")
 
-class PerformSkim(luigi.Task):
-    dataset = luigi.Parameter()
-    
-    def run(self):
-        print(skim().cmssw_base)
+    def requires(self):
+        yield ExtractTarMember(tarpath=os.path.join(self.crab_path,"inputs/debugFiles.tgz"),
+                                member="debug/crabConfig.py")
+
+class RegisterCrabFiles(sqla.CopyToTable):
+    crab_path = luigi.Parameter()
+
+    reflect = True
+    connection_string = "sqlite:///test.db"
+    table = "file"
+    column_separator=" "
+    def requires(self):
+        return GetCrabFiles(self.crab_path)
 
 if __name__ == '__main__':
 
@@ -122,5 +161,5 @@ if __name__ == '__main__':
 
     # tasks = []
 
-    task = ExtractTarFile(filepath="/disk1/albert/hinv_vbf/slc6/CMSSW_10_2_11/src/PhysicsTools/NanoAODTools/crab/wdir/19Mar19/crab_nano_post_19Mar19_SingleElectron_Run2017C/inputs/debugFiles.tgz")
+    task = RegisterCrabFiles(crab_path="/disk1/albert/hinv_vbf/slc6/CMSSW_10_2_11/src/PhysicsTools/NanoAODTools/crab/wdir/19Mar19/crab_nano_post_19Mar19_SingleElectron_Run2017C/")
     luigi.build([task], local_scheduler=True)
