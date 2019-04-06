742da293684cbc7d6f21aaf7d609b866eb27e9f2
Andreas Albert <andreas.albert@cern.ch>
2019-04-06 22:50:48 +0200

    WIP: runner rework

--- scripts/trybind.py
+++ scripts/trybind.py
@@ -3,49 +3,96 @@ import sys
 import PyBindings
 import argparse
 
-def get_input_files():
-    nargs = len(sys.argv)
 
-    if nargs==2 and sys.argv[1].endswith(".txt"):
-        with open(sys.argv[1],"r") as f:
-            files = [x.strip() for x in f.readlines()]
-    elif nargs > 1:
-        files = sys.argv[1:]
-    else:
-        files = []
-        files.append("/home/albert/repos/nanocppfw/data/007E0986-34E9-9741-A447-957FF2F1982C.root");
-        files.append("/home/albert/repos/nanocppfw/data/78341E3C-F2BD-A64E-95D6-550FCA8DDAD4.root");
-        files.append("/home/albert/repos/nanocppfw/data/E4D76467-890E-E811-8523-FA163E3D9AF7.root");
-    return files
+from database_objects import *
+import sqlalchemy as db
 
-def is_valid_dataset(dataset):
-    forbidden = "\#|@;:'\"\',<.>/?"
-    if any([x in dataset for x in forbidden]):
-        return False
-    return True
+from sqlalchemy.orm import sessionmaker
+import re
 
+def init_db_session(path_to_db):
+    engine = db.create_engine('sqlite:///' + path_to_db)
+
+    Session = sessionmaker(bind=engine)
+
+    return Session()
 
 def parse_cli():
     parser = argparse.ArgumentParser(description='Runs HInv Analysis')
-    parser.add_argument('files',type=str, nargs='+',
-                        help='Input files to run over')
-    parser.add_argument('--dataset', default=None, required=True, type=str,
+
+    subparsers = parser.add_subparsers()
+
+    analysis_args = parser.add_argument_group("Analysis")
+    analysis_args.add_argument('--analyzer', default="HInvAnalyzer", type=str,
+                        help='Analyzer to run.')
+
+    input_args = parser.add_argument_group("Inputs")
+    input_args.add_argument('--dataset', default=None, required=True, type=str,
                         help='Dataset name.')
+
+    input_args.add_argument('--files', default=None, type=str, nargs='+',
+                        help='Set file list manually.')
+
+    input_args.add_argument('--db', default="../python/database/test.db", type=str,
+                        help='Database file to use.')
+
+    input_args.add_argument('--skim', default="19Mar19", type=str,
+                        help='Skim to run over.')
+    input_args.add_argument('--period', default="2017", type=str,
+                        help='Period to run over.')
+
+
+    parser_local = subparsers.add_parser("local")
+    parser_local.set_defaults(func=do_local)
+
+    parser_sub = subparsers.add_parser("submit")
+    parser_local.set_defaults(func=do_submit)
 
     args = parser.parse_args()
 
-    if not is_valid_dataset(args.dataset):
-        raise ArgumentError("Invalid dataset encountered: '{}'.".format(args.dataset))
+
+
+
 
     return args
 
 
+def do_local(args):
+    # Initialize right type of analyzer
+    Analyzer = getattr(PyBindings, args.analyzer)
+
+    if args.files:
+        # Manual mode
+        ana = Analyzer(args.files)
+        ana.set_fixed_dataset(args.dataset)
+        ana.run()
+    else:
+        # Database mode
+        # /disk1/albert/hinv_vbf/slc7/analysis/nanocppfw/python/database/test.db
+        session = init_db_session(args.db)
+
+        for dataset in session.query(Dataset):
+
+            # Period and dataset name must match
+            match = re.match(args.period, dataset.period) \
+                    and re.match(args.dataset, dataset.path)
+            if not match:
+                continue
+
+            # Files from right skim
+            files = [x for x in dataset.files if x.skim_tag == args.skim]
+
+            ana = Analyzer(files)
+            ana.set_fixed_dataset(args.dataset)
+            ana.run()
+    return
+
+def do_submit(args):
+    return
+
 def main():
-    # files = get_input_files()
     args = parse_cli()
-    ana = PyBindings.HInvAnalyzer(args.files)
-    ana.set_fixed_dataset(args.dataset)
-    ana.run()
+
 
 if __name__ == "__main__":
     main()
