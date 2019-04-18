d5d91943cc65e01d332f4271d4e9dc41d4ff426e
Andreas Albert <andreas.albert@cern.ch>
2019-04-18 14:30:06 +0200

    Work on glite task

--- nanocppfw/scripts/grid_bootstrap.sh
+++ nanocppfw/scripts/grid_bootstrap.sh
@@ -3,13 +3,13 @@ load_replica() {
     local bundle_re="$2"
     local arc_path="$3"
 
-    local arc="$( gfal-ls "$remote_base" | grep -Po "$bundle_re" | shuf -n 1 )"
+    local arc="$( python $(which gfal-ls) "$remote_base" | grep -Po "$bundle_re" | shuf -n 1 )"
     if [ -z "$arc" ]; then
         >&2 echo "could not determine archive to load from $remote_base"
         return "1"
     fi
 
-    gfal-copy "$remote_base/$arc" "$arc_path"
+    python $(which gfal-copy) "$remote_base/$arc" "$arc_path"
     if [ "$?" != "0" ]; then
         >&2 echo "could not load archive $arc from $remote_base"
         return "1"
@@ -17,19 +17,23 @@ load_replica() {
 }
 
 
-export CMSSW_VERSION="CMSSW_10_2_13"
+# export CMSSW_VERSION="CMSSW_10_5_0"
 export ENVNAME=myenv
 
-### Set up CMSSW
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-scramv1 project CMSSW $CMSSW_VERSION
-pushd $CMSSW_VERSION/src;
-eval `scramv1 runtime -sh`
-popd
+# ### Set up CMSSW
+# source /cvmfs/cms.cern.ch/cmsset_default.sh
+# scramv1 project CMSSW $CMSSW_VERSION
+# pushd $CMSSW_VERSION/src;
+# eval `scramv1 runtime -sh`
+# popd
 
+
+# source /cvmfs/sft.cern.ch/lcg/views/ROOT-latest/x86_64-slc6-gcc7-opt/setup.sh
+# source /cvmfs/grid.cern.ch/centos7-wn-4.0.5-1_umd4v1/etc/profile.d/setup-c7-wn-example.sh
 
 #### Virtual environment
-python -m virtualenv "${ENVNAME}"
+python -m pip install --user virtualenv
+python -m virtualenv "${ENVNAME}" -p $(which python)
 export ENVDIR=$(readlink -e "./${ENVNAME}/")
 source ${ENVDIR}/bin/activate
 
@@ -40,8 +44,16 @@ python -m pip install law
 mkdir -p software
 pushd software
 
+
+# # Set up gfal
+# wget https://www.dropbox.com/s/3nylghi0xtqaiyy/gfal2.tgz
+# tar -xzf gfal2.tgz
+# rm gfal2.tgz
+
+# source "gfal2/setup.sh" || return "$?"
+
 # The framework
-load_replica "srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=/pnfs/physik.rwth-aachen.de/cms/store/user/aalbert/testlaw/" "nanocppfw.*.tgz" "nanocppfw.tgz"
+load_replica "${GRIDPACK_PATH}" $(echo ${GRIDPACK_NAME} | sed "s|\.tgz|.*.tgz|") "nanocppfw.tgz"
 
 tar xf nanocppfw.tgz  || return "$?"
 rm nanocppfw.tgz
@@ -53,12 +65,6 @@ popd
 
 export LUIGI_CONFIG_PATH="$NANOCPPFW_BASE/nanocppfw/law.cfg)"
 
-# Set up gfal
-wget https://www.dropbox.com/s/3nylghi0xtqaiyy/gfal2.tgz
-tar -xzf gfal2.tgz
-rm gfal2.tgz
-
-source "gfal2/setup.sh" || return "$?"
 popd
 ls -la software
 ls -la .
--- nanocppfw/tasks.py
+++ nanocppfw/tasks.py
@@ -13,30 +13,31 @@ from law.workflow.local import LocalWorkflow
 from law.contrib.git import BundleGitRepository
 
 from nanocppfw.pybindings import HInvAnalyzer
-__all__ = ["ExampleAnalysisTask"]
+__all__ = ["ExampleAnalysisTask","NanoCPPFWUpload"]
 
 class NanoCPPFWGLiteWorkflow(LocalWorkflow, GLiteWorkflow):
 
     def glite_bootstrap_file(self):
-        return law.util.rel_path(__file__, "../scripts/grid_bootstrap.sh")
+        return law.util.rel_path(__file__, "./scripts/grid_bootstrap.sh")
 
     def glite_output_directory(self):
         return WLCGDirectoryTarget(path="subfolder")
 
     def glite_output_uri(self):
         return self.glite_output_directory().url(cmd="listdir")
-        # return self.glite_output_directory()
+
+    def glite_workflow_requires(self):
+        return {"software" : NanoCPPFWUpload(source_path="", replicas=1)}
+
+    def _setup_render_variables(self, config, reqs):
+        config.render_variables["GRIDPACK_NAME"] = self.glite_workflow_requires()["software"].single_output().path.replace("/","")
+        config.render_variables["GRIDPACK_PATH"] = self.glite_workflow_requires()["software"].output().dir.url()
 
     def glite_job_config(self, config, job_num, branches):
         config = law.GLiteWorkflow.glite_job_config(self, config, job_num, branches)
+        self._setup_render_variables(config, self.glite_workflow_requires())
         config.vo = "cms:/cms/dcms"
         return config
-
-    def glite_workflow_requires(self):
-        reqs = {}
-        reqs["bundle"] = BundleGitRepositoryWithDateStamp.req(self,repo_path=os.environ["NANOCPPFW_BASE"], exclude_files=["*.tgz","data"])
-        reqs["software"] = UploadSoftware.req(self, source_path=reqs["bundle"].output().path,replicas=5, _prefer_cli=["replicas"])
-        return reqs
 
 from datetime import datetime
 class BundleGitRepositoryWithDateStamp(BundleGitRepository):
@@ -53,11 +54,22 @@ class UploadSoftware(TransferLocalFile):
         return WLCGFileTarget(os.path.basename(self.source_path))
 
 
+class NanoCPPFWUpload(UploadSoftware):
+
+    # def run(self):
+
+    def requires(self):
+        return BundleGitRepositoryWithDateStamp(repo_path=os.environ["NANOCPPFW_BASE"], exclude_files=["*.tgz","data"])
+    def single_output(self):
+        return WLCGFileTarget(os.path.basename(self.requires().output().path))
+
+
 class ExampleAnalysisTask(NanoCPPFWGLiteWorkflow):
 
     def create_branch_map(self):
-        return {1 : ["root://cms-xrd-global.cern.ch///store/mc/RunIIFall17NanoAODv4/ZJetsToNuNu_HT-200To400_13TeV-madgraph/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/10000/1BE8C84A-D732-B345-B208-F39E9C339BA0.root"],
-        2:["root://cms-xrd-global.cern.ch///store/mc/RunIIFall17NanoAODv4/ZJetsToNuNu_HT-2500ToInf_13TeV-madgraph/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/20000/884A635C-ADF3-184A-8309-185474851091.root"]}
+        return {1 : ["root://cms-xrd-global.cern.ch///store/mc/RunIIFall17NanoAODv4/ZJetsToNuNu_HT-100To200_13TeV-madgraph/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/90000/007E0986-34E9-9741-A447-957FF2F1982C.root"],
+        # 2:["root://cms-xrd-global.cern.ch///store/mc/RunIIFall17NanoAODv4/ZJetsToNuNu_HT-600To800_13TeV-madgraph/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/90000/78341E3C-F2BD-A64E-95D6-550FCA8DDAD4.root"]
+        }
 
     def run(self):
         inputdata = self.branch_data
@@ -68,9 +80,5 @@ class ExampleAnalysisTask(NanoCPPFWGLiteWorkflow):
 
     def output(self):
         output_name = "output_HInvAnalyzer_{}.root".format(self.branch)
-        try:
-            import gfal2
-            return WLCGFileTarget(output_name)
-        except ImportError:
-            return LocalFileTarget(output_name)
+        return WLCGFileTarget(output_name)
 
