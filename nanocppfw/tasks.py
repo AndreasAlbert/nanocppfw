00028ce7de8ed7461133b55bb44f926f4033dd3d
Andreas Albert <andreas.albert@cern.ch>
2019-04-18 09:33:13 +0200

    Implement analysis task.

--- nanocppfw/tasks.py
+++ nanocppfw/tasks.py
@@ -1,5 +1,5 @@
 import os
-
+import luigi
 
 import law
 law.contrib.load("glite")
@@ -15,9 +15,7 @@ from law.contrib.git import BundleGitRepository
 from nanocppfw.pybindings import HInvAnalyzer
 __all__ = ["ExampleAnalysisTask"]
 
-class AnalysisBaseTask(LocalWorkflow, GLiteWorkflow):
-    def output(self):
-        return WLCGFileTarget("output_{}.txt".format(self.branch))
+class NanoCPPFWGLiteWorkflow(LocalWorkflow, GLiteWorkflow):
 
     def glite_bootstrap_file(self):
         return law.util.rel_path(__file__, "../scripts/grid_bootstrap.sh")
@@ -55,17 +53,24 @@ class UploadSoftware(TransferLocalFile):
         return WLCGFileTarget(os.path.basename(self.source_path))
 
 
-class ExampleAnalysisTask(AnalysisBaseTask):
+class ExampleAnalysisTask(NanoCPPFWGLiteWorkflow):
+
     def create_branch_map(self):
         return {1 : ["root://cms-xrd-global.cern.ch///store/mc/RunIIFall17NanoAODv4/ZJetsToNuNu_HT-200To400_13TeV-madgraph/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/10000/1BE8C84A-D732-B345-B208-F39E9C339BA0.root"],
-        2:["/store/mc/RunIIFall17NanoAODv4/ZJetsToNuNu_HT-2500ToInf_13TeV-madgraph/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/20000/884A635C-ADF3-184A-8309-185474851091.root"]}
+        2:["root://cms-xrd-global.cern.ch///store/mc/RunIIFall17NanoAODv4/ZJetsToNuNu_HT-2500ToInf_13TeV-madgraph/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/20000/884A635C-ADF3-184A-8309-185474851091.root"]}
 
     def run(self):
         inputdata = self.branch_data
         ana = HInvAnalyzer(inputdata)
         ana.set_fixed_dataset("dummy")
+        ana.set_output_path(self.output().path)
         ana.run()
 
     def output(self):
-        return LocalFileTarget("output.root")
-        # output.dump({"input":inputdata, "output":outputdata})
+        output_name = "output_HInvAnalyzer_{}.root".format(self.branch)
+        try:
+            import gfal2
+            return WLCGFileTarget(output_name)
+        except ImportError:
+            return LocalFileTarget(output_name)
+
