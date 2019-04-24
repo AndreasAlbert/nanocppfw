43de3de1b75f281ed344fa8ac7f37b338dcd4274
albert <albert@physik.rwth-aachen.de>
2019-04-24 14:00:43 +0200

    Improve setup

--- nanocppfw/htcondor/scripts/htcondor_cern_bootstrap.sh
+++ nanocppfw/htcondor/scripts/htcondor_cern_bootstrap.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 action() {
+    echo "Sourcing setup.sh"
     source "${NANOCPPFW_BASE}/setup.sh"
+
+    echo "Initiating VOMS proxy."
     export X509_USER_PROXY=$(readlink -e ./x509up*)
+
+    echo "Sourcing virtual environment"
+    source "${VIRTUAL_ENV}/bin/activate"
 }
 action
--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
+# Analysis base directory
 export NANOCPPFW_BASE=$(readlink -e $(dirname "${BASH_SOURCE[0]}"))
+export NANOCPPFW_DATABASE="${NANOCPPFW_BASE}/test.db"
+
+# Path for data storage
 export ANALYSIS_DATA_PATH="${NANOCPPFW_BASE}/data/"
+
+# Python path
 export PYTHONPATH=${PYTHONPATH}:"$NANOCPPFW_BASE":"$NANOCPPFW_BASE/nanocppfw"
+
+# luigi
+export LUIGI_CONFIG_FILE=${NANOCPPFW_BASE}/luigi.cfg
+
+# law
+export LAW_HOME="${NANOCPPFW_BASE}/.law"
 export LAW_CONFIG_FILE=${NANOCPPFW_BASE}/nanocppfw/law.cfg
