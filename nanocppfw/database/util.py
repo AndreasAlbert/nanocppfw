c340c4a236dbdf77964897cd4ef97e46f1971a7b
Andreas Albert <andreas.albert@cern.ch>
2019-04-25 12:35:37 +0200

    Improve CLI handling

--- nanocppfw/database/util.py
+++ nanocppfw/database/util.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 from nanocppfw.database.database_objects import Dataset
 
+
 def init_db_session(path_to_db):
     engine = db.create_engine('sqlite:///' + path_to_db)
 
@@ -13,10 +14,11 @@ def init_db_session(path_to_db):
 
     return Session()
 
-class DatabaseInterface():
+
+class DatabaseInterface(object):
     """Common interfacing for accessing and filtering objects from the database"""
 
-    def __init__(self, path_to_db=None, filter_period=".*", filter_dataset_path=".*", filter_dataset_shortname=".*"):
+    def __init__(self, args=None, path_to_db=None, **kwargs):
         # Initialize session
         if path_to_db is None:
             self.path_to_db = os.environ["NANOCPPFW_DATABASE"]
@@ -25,18 +27,26 @@ class DatabaseInterface():
         self.session = init_db_session(self.path_to_db)
 
         # Allow filtering by period, path and short name
-        self.filter_period = filter_period
-        self.filter_dataset_path = filter_dataset_path
-        self.filter_dataset_shortname = filter_dataset_shortname
+        self.filters = {}
+        for filter_name in self.known_filter_names():
+            optname = self.filter_name_to_cli_arg(filter_name).replace("-","_").strip("_")
+            
+            value = getattr(args, optname, self.default_filter())
+            self.filters[filter_name] = value
+
+        # Allow manual override of options
+        for key, value in kwargs.items():
+            if key.startswith("filter_"):
+                setattr(self, key, value)
 
     def _dataset_matches_period(self, dataset):
-        return re.match(self.filter_period, dataset.period_tag)
+        return re.match(self.filters["filter_period"], dataset.period_tag)
 
     def _dataset_matches_path(self, dataset):
-        return re.match(self.filter_dataset_path, dataset.path)
+        return re.match(self.filters["filter_dataset_path"], dataset.path)
 
     def _dataset_matches_shortname(self, dataset):
-        return re.match(self.filter_dataset_shortname, dataset.shortname)
+        return re.match(self.filters["filter_dataset_shortname"], dataset.shortname)
 
     def _dataset_matches_all(self, dataset):
         return self._dataset_matches_period(dataset) \
@@ -45,3 +55,32 @@ class DatabaseInterface():
 
     def get_datasets(self):
         return filter(self._dataset_matches_all, self.session.query(Dataset))
+
+    @classmethod
+    def known_filter_names(self):
+        return [
+            "filter_period",
+            "filter_dataset_path",
+            "filter_dataset_shortname"
+        ]
+
+    @classmethod
+    def filter_name_to_cli_arg(self, filter_name):
+        return "--db-"+filter_name.replace("_","-")
+
+    @classmethod
+    def default_filter(self):
+        return ".*"
+
+    @classmethod
+    def add_cli_parsing(self, parser):
+        dbargs = parser.add_argument_group("DatabaseInterface")
+        for filter_name in self.known_filter_names():
+            dbargs.add_argument(self.filter_name_to_cli_arg(filter_name), default=self.default_filter(), type=str,
+                                 help="Regular expression to filter by {}".format(filter_name.replace("filter-", "")))
+        # dbargs.add_argument('--db-filter-period', default=".*", type=str,
+        #                     help='Regular expression to filter by period.')
+        # dbargs.add_argument('--db-filter-dataset-path', default=".*", type=str,
+        #                     help='Regular expression to filter by dataset path.')
+        # dbargs.add_argument('--db-filter-dataset-shortname', default=".*", type=str,
+        #                     help='Regular expression to filter by dataset short name.')
